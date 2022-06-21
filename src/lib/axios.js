fbeaebba9c30cb873e06051e33e855a2d3abab57
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2022-06-21 19:29:20 -0300

    install js-cookie

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,4 +1,13 @@
 import Axios from 'axios'
+import Cookies from 'js-cookie'
+
+// const axios = Axios.create({
+//     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+//     headers: {
+//         'X-Requested-With': 'XMLHttpRequest',
+//     },
+//     withCredentials: true,
+// })
 
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
@@ -7,5 +16,26 @@ const axios = Axios.create({
     },
     withCredentials: true,
 })
+
+axios.defaults.withCredentials = true
+
+const onRequest = config => {
+    console.log('setCSRFToken')
+    console.log(Cookies.get('XSRF-TOKEN'))
+    if (
+        config.method == 'post' ||
+        config.method == 'put' ||
+        (config.method == 'delete' && !Cookies.get('XSRF-TOKEN'))
+    ) {
+        return setCSRFToken().then(response => config)
+    }
+    return config
+}
+
+const setCSRFToken = () => {
+    return axios.get('/sanctum/csrf-cookie')
+}
+
+axios.interceptors.request.use(onRequest, null)
 
 export default axios
