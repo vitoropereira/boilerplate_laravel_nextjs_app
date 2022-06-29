28b43a64883c210d03e6256f14da87c7bc8f3fd6
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2022-06-29 09:19:13 -0300

    Ajusment axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,40 +1,40 @@
 import Axios from 'axios'
 import Cookies from 'js-cookie'
 
-const axios = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-    },
-    withCredentials: true,
-})
-
 // const axios = Axios.create({
 //     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 //     headers: {
 //         'X-Requested-With': 'XMLHttpRequest',
 //     },
+//     withCredentials: true,
 // })
 
-// axios.defaults.withCredentials = true
+const axios = Axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+    },
+})
 
-// const onRequest = config => {
-//     console.log('setCSRFToken')
-//     console.log(Cookies.get('XSRF-TOKEN'))
-//     if (
-//         config.method == 'post' ||
-//         config.method == 'put' ||
-//         (config.method == 'delete' && !Cookies.get('XSRF-TOKEN'))
-//     ) {
-//         return setCSRFToken().then(response => config)
-//     }
-//     return config
-// }
+axios.defaults.withCredentials = true
 
-// const setCSRFToken = () => {
-//     return axios.get('/sanctum/csrf-cookie')
-// }
+const onRequest = config => {
+    console.log('setCSRFToken')
+    console.log(Cookies.get('XSRF-TOKEN'))
+    if (
+        config.method == 'post' ||
+        config.method == 'put' ||
+        (config.method == 'delete' && !Cookies.get('XSRF-TOKEN'))
+    ) {
+        return setCSRFToken().then(response => config)
+    }
+    return config
+}
 
-// axios.interceptors.request.use(onRequest, null)
+const setCSRFToken = () => {
+    return axios.get('/sanctum/csrf-cookie')
+}
+
+axios.interceptors.request.use(onRequest, null)
 
 export default axios
