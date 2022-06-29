fed048d56562262bfdf101a48868d207ec59884a
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2022-06-29 18:45:12 -0300

    Ajust csrfToken console.log

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -17,7 +17,11 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
         }),
     )
 
-    const csrf = () => axios.get('/sanctum/csrf-cookie')
+    const csrf = () =>
+        axios.get('/sanctum/csrf-cookie').then(res => {
+            console.log('res.data')
+            console.log(res.data)
+        })
 
     const register = async({ setErrors, ...props }) => {
         await csrf()
